5ba87b31b57a5bd65c8687c34151abf74b41fae2
matheus-paulino <matheuspaulino.santos1@gmail.com>
2021-02-22 21:28:05 -0300

    use widget column for organize widgets

--- lib/main.dart
+++ lib/main.dart
@@ -6,13 +6,34 @@ main() {
 
 @override
 class AskApp extends StatelessWidget {
+  final List<String> questions = [
+    'Qual é a sua cor favorita?',
+    'Qual é o seu animal favorito'
+  ];
+
   Widget build(BuildContext context) {
     return new MaterialApp(
       home: new Scaffold(
         appBar: new AppBar(
           title: Text('Questions'),
         ),
-        body: Text('Any questions'),
+        body: new Column(
+          children: <Widget>[
+            new Text(questions[0]),
+            new RaisedButton(
+              child: new Text('Reposta 01'),
+              onPressed: () {},
+            ),
+            new RaisedButton(
+              child: new Text('Reposta 02'),
+              onPressed: () {},
+            ),
+            new RaisedButton(
+              child: new Text('Reposta 03'),
+              onPressed: () {},
+            ),
+          ],
+        ),
       ),
     );
   }
