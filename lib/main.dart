a16b352992b8313dd8f238a9c4e6324c5e327784
matheus-paulino <matheuspaulino.santos1@gmail.com>
2021-02-27 13:51:44 -0300

    avoid invalid question

--- lib/main.dart
+++ lib/main.dart
@@ -10,28 +10,38 @@ main() {
 class QuestionAppState extends State<AskApp> {
   int _askSelected = 0;
 
+  final List<Map<String, Object>> _questions = const [
+    {
+      'question': 'Qual é sua cor favorita?',
+      'reponses': ['Preto', 'Vermelho', 'Verde', 'Branco'],
+    },
+    {
+      'question': 'Qual é seu animal favorito?',
+      'reponses': ['Cachorro', 'Girafa', 'Gato', 'Passáro'],
+    }
+  ];
+
   void _response() {
-    setState(() {
-      _askSelected++;
-    });
+    if (hasQuestionSelected) {
+      setState(() {
+        _askSelected++;
+      });
+    }
+
+    print(_askSelected);
+  }
+
+  bool get hasQuestionSelected {
+    return _askSelected < _questions.length;
   }
 
   @override
   Widget build(BuildContext context) {
-    final List<Map<String, Object>> questions = [
-      {
-        'question': 'Qual é sua cor favorita?',
-        'reponses': ['Preto', 'Vermelho', 'Verde', 'Branco'],
-      },
-      {
-        'question': 'Qual é seu animal favorito?',
-        'reponses': ['Cachorro', 'Girafa', 'Gato', 'Passáro'],
-      }
-    ];
+    List<String> responses =
+        hasQuestionSelected ? _questions[_askSelected]['reponses'] : null;
 
-    List<String> responses = questions[_askSelected]['reponses'];
-    List<Widget> answersWidgets =
-        responses.map((r) => Answer(r, _response)).toList();
+    /* List<Widget> answersWidgets =
+        responses.map((r) => Answer(r, _response)).toList(); */
     /* for (String questionAnswer in responses) {
       answersWidgets.add(Answer(questionAnswer, _response));
     } */
@@ -41,12 +51,14 @@ class QuestionAppState extends State<AskApp> {
         appBar: new AppBar(
           title: Text('Questions'),
         ),
-        body: new Column(
-          children: <Widget>[
-            new Question(questions[_askSelected]['question']),
-            ...answersWidgets
-          ],
-        ),
+        body: hasQuestionSelected
+            ? Column(
+                children: <Widget>[
+                  new Question(_questions[_askSelected]['question']),
+                  ...responses.map((t) => Answer(t, _response)).toList()
+                ],
+              )
+            : null,
       ),
     );
   }
